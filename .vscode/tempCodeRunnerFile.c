c1d4a48fae3bfedc70a2a29424a71107b6093d2c
indumathisena <velamurisena@gmail.com>
2023-11-15 10:40:37 +0530

    commit

---
+++ .vscode/tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    int number ;
+    printf("Enter an integer: ");
+    // read and stores input
+    scanf("%d", &number);
+    //displays output
+    printf("You enterd:%d",number);
+    return 0;
+}
---
+++ sixth.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    a=10;
+    printf("valueof a:%d",a);
+    return 0;
+}
